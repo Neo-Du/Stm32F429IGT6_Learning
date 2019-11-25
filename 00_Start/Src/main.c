d524cc93ab621877792f729a2539845430542fe1
Du <duzlvip@gmail.com>
2019-11-26 00:44:29 +0800

    add keyInput and Buzzer

--- 00_Start/Src/main.c
+++ 00_Start/Src/main.c
@@ -89,7 +89,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-
+  printf("Start\r\n");
   /* USER CODE END 2 */
 
   /* Infinite loop */
